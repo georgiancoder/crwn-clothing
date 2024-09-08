c8e93fe9916b671508d0064747eaa9e0f46f2ca9
b.tsalani <b.tsalani@adjarabet.com>
2024-09-08 23:41:21 +0400

    replace context with redux

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
             if (user) createUserDocumentFromAuth(user)
             dispatch(setCurrentUser(user));
         });
+        // eslint-disable-next-line
     }, [])
 
     return (
