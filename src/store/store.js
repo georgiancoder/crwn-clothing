0424d5dc39aa2e32a78053a2523e8520cd5c2066
b.tsalani <b.tsalani@adjarabet.com>
2024-09-09 13:51:32 +0400

    persistor

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,22 @@
 import {applyMiddleware, compose, createStore} from "redux";
 import {rootReducer} from "./root-reducer";
 import {logger} from "redux-logger/src";
+import storage from "redux-persist/es/storage";
+import {persistReducer, persistStore} from "redux-persist";
 
-const middlewares = [logger];
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const persistor = persistStore(store);
